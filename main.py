c41eaf14245c5fa06478fe0d4ce5245543b6e112
ABochik <ab@el-pro.ru>
2023-06-13 11:45:10 +0300

    Some unused func cleared

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import time
 import pandas as pd
 import queue
 import searcher_new as searcher
-import math
 import data.config as config
 from PySimpleGUI import PopupGetFile
 
@@ -29,69 +28,42 @@ def pop_list(list_to_pop, num):
 def main(work: [queue.SimpleQueue, list], break_after_first: bool, thread_col: int, doubling: bool, file):
     proxies = config.proxies * 2 if doubling else config.proxies
     proxies = proxies[:thread_col] if (thread_col != 0) and (thread_col > 0) else proxies
-    if type(work) is list:
-        assert len(work) > 0, 'Ошибка очереди заданий'
-        work_len = math.ceil(len(work)/len(proxies))
-        data = []
-        works = [
-            threading.Thread(
-                target=function_to_thread,
-                args=(search, (pop_list(work, work_len), proxy), data)
-            ) for proxy in proxies
-        ]
-        for w in works:
-            w.start()
-            time.sleep(1)
-        for w in works:
-            w.join()
-        if data:
-            frame = data.pop()
-            for appended in data:
-                frame = frame.append(appended)
-            frame.to_excel('result.xlsx')
-    else:
-        assert not work.empty(), 'Ошибка очереди заданий'
-        data = []
-        works = [
-            threading.Thread(
-                target=search_queue,
-                args=(work, proxy, break_after_first, data)
-            ) for proxy in proxies
-        ]
-        wait = 5
-        for w in works:
-            w.start()
-            if wait:
-                time.sleep(wait)
-            wait = 0
-        for w in works:
-            w.join()
-        if not queue.empty():
-            errors = 1
-            while errors < 5 and (not queue.empty()):
-                works = [
-                    threading.Thread(
-                        target=search_queue,
-                        args=(work, proxy, break_after_first, data)
-                    ) for proxy in proxies
-                ]
-                for w in works:
-                    w.start()
-                for w in works:
-                    w.join()
-
-        if data:
-            frame, not_found = data.pop()
-            for appended, not_found_appended in data:
-                frame = frame.append(appended)
-                not_found = not_found.append(not_found_appended)
-            frame.to_excel('result.xlsx')
-            not_found.to_excel('not_found.xlsx')
-
-
-def search(work, proxy):
-    searcher_bot = searcher.Searcher(proxy=proxy)
-    return searcher_bot.start(work, )
+    assert not work.empty(), 'Ошибка очереди заданий'
+    data = []
+    works = [
+        threading.Thread(
+            target=search_queue,
+            args=(work, proxy, break_after_first, data)
+        ) for proxy in proxies
+    ]
+    wait = 5
+    for w in works:
+        w.start()
+        if wait:
+            time.sleep(wait)
+        wait = 0
+    for w in works:
+        w.join()
+    if not queue.empty():
+        errors = 1
+        while errors < 5 and (not queue.empty()):
+            works = [
+                threading.Thread(
+                    target=search_queue,
+                    args=(work, proxy, break_after_first, data)
+                ) for proxy in proxies
+            ]
+            for w in works:
+                w.start()
+            for w in works:
+                w.join()
+    if data:
+        frame, not_found = data.pop()
+        for appended, not_found_appended in data:
+            frame = frame.append(appended)
+            not_found = not_found.append(not_found_appended)
+        frame.to_excel('result.xlsx')
+        not_found.to_excel('not_found.xlsx')
 
 
 def search_queue(work, proxy, break_after_first, data):
